c55c6ea87599d7fe3a6a0376aaa2d62975c897ce
vegetakunssj4 <jarvoop6969@gmail.com>
2025-03-14 15:21:21 +0530

    Update pro.py

--- pro.py
+++ pro.py
@@ -246,5 +246,8 @@ async def main():
     await idle()  # Keep bot running
     await bot.stop()
 
+
+
 if __name__ == "__main__":
-    asyncio.run(main())
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
