b42e4db1eaeada523f0195c454fafbd9b504fa78
jarvoop55 <swamiyeola153@gmail.com>
2025-06-15 20:46:24 +0530

    Update son.py

--- son.py
+++ son.py
@@ -93,7 +93,7 @@ TARGET_GROUP_IDS = [
     -1002560135170,
     -1002389233447
 ] # Target groups
-MAIN_GROUP_ID = -1002348881334  # Main group for /startmain command
+MAIN_GROUP_ID = -1002482984360  # Main group for /startmain command
 FORWARD_CHANNEL_ID = -1002260368357 # Forwarding channel
 
 # Track collection status for each group
